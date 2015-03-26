15518cbf70756f170803a80c830b0b58702e0517
Viktor Gubriienko <viktor@swivl.com>
2015-03-26 17:26:30 +0200

    Initial commit

---
+++ SFDispatchAfter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFDispatchAfter
+//
+//  Created by Viktor Gubriienko on 3/26/15.
+//  Copyright (c) 2015 Viktor Gubriienko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
