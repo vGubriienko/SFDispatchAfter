5cf896c5f27c483ab68e66fde0c965904ea9a740
Viktor Gubriienko <viktor@swivl.com>
2015-10-28 16:13:25 +0200

    Updated project to swift 2.0 Make shared schema

--- SFDispatchAfter/AppDelegate.swift
+++ SFDispatchAfter/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        var obj1 = SFDispatchObject.dispatchAfter(3.0) {
+        let obj1 = SFDispatchObject.dispatchAfter(3.0) {
             NSLog("Should never be called")
         }
         
